987fbdcaad232bb1a72a219caa58cc0b674b36cd
frglezd <ragodrone@gmail.com>
2017-08-07 09:37:05 -0600

    changes made

---
+++ ch01/swap.java
@@ -0,0 +1,17 @@
+import java.util.*;
+
+class swap{
+
+	public static void swap(int a, int b){
+		int temp = a;
+		a = b;
+		b = temp;
+		return a;
+		return b;
+	}
+
+	public static void main(String[] args){
+		System.out.println(swap(5, 8));
+
+	}
+}
