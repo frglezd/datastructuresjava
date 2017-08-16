ac5cfe49b7b0b1068d233355436c29c7209779f6
frglezd <ragodrone@openmailbox.org>
2017-08-16 10:56:48 -0600

    changes made

--- schaum/ch04/TestPriorityQueue.java
+++ schaum/ch04/TestPriorityQueue.java
@@ -2,8 +2,6 @@ import java.util.*;
 
 public class TestPriorityQueue {
 	public static void main(String[] args) {
-		long startTime = System.currentTimeMillis();
-
 		Queue<String> queue = new PriorityQueue<String>();
 		Collections.addAll(queue, "CO", "UY", "EC", "AR");
 		System.out.println(queue);
@@ -16,8 +14,5 @@ public class TestPriorityQueue {
 		System.out.printf("Removed %s%n", queue.remove());
 		System.out.printf("Removed %s%n", queue.remove());
 		System.out.println(queue);
-		
-		long endTime = System.currentTimeMillis();
-		System.out.println("Time " + (endTime - startTime) + " milliseconds");
 	}
 }
