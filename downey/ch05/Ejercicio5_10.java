5717ae02eb3521de1be81c4323f2ae9336fd848c
frglezd <ragodrone@gmail.com>
2017-09-09 13:32:07 -0600

    changes made

---
+++ downey/ch05/Ejercicio5_10.java
@@ -0,0 +1,22 @@
+import java.util.*;
+
+class Ejercicio5_10{
+
+    public static double potencia(double x, double n){
+        if(n == 0){
+            return 1.0;
+        }
+        else if (n > 0){
+            return x * potencia(x, n-1);
+        }
+        else{
+            return 0;
+        }
+    }
+
+    public static void main(String[] args){
+        System.out.println(potencia(2,3));
+
+    }
+
+}
---
+++ downey/ch06/RaizCuadrada.java
@@ -0,0 +1,30 @@
+import java.util.*;
+
+class RaizCuadrada{
+
+    public static double raizCuadrada(double x){
+        
+        double temp, a;
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Ingrese un valor inicial:");
+        double xi = sc.nextDouble();
+       
+        while(true){
+        temp = xi;
+        a = (xi + x/xi); 
+        xi = a/2;
+            if(Math.abs(xi-temp) < 0.0001){
+                System.out.println("Raiz cuadrada por aproximación: " + xi);
+                return 0;
+            }
+        }
+
+    }
+
+    public static void main(String[] args){
+
+        raizCuadrada(9);
+        System.out.println("Raiz cuadrada por Math.sqrt: " + Math.sqrt(9));
+
+    }
+}
