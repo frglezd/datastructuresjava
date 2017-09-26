accf1826b178597a50ddd5113c1fff829c75fcb0
Francisco Gonzalez <frglezd@gmail.com>
2017-09-26 10:12:06 -0600

    changes for new version of eclipse

--- downey/ch03/Ejercicio3_4.java
+++ downey/ch03/Ejercicio3_4.java
@@ -2,14 +2,14 @@
 
 class Ejercicio3_4{
 
-    public static void imprimirFecha(String diaSemana, String diaMes, String mes, int aÃ±o){
+    public static void imprimirFecha(String diaSemana, String diaMes, String mes, int año){
         System.out.print(diaSemana + ", ");
         System.out.print(mes + " ");
         System.out.print(diaMes + " ,");
-        System.out.println(aÃ±o);
+        System.out.println(año);
     }
     
     public static void main(String[] args){
-        imprimirFecha("MiÃ©rcoles", "29", "Septiembre", 2010);
+        imprimirFecha("Miércoles", "29", "Septiembre", 2010);
     }
 }
